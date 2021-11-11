f1f997b1e1f74e828e7ce9ef7173a1a43be7186c
ManoelCabral1 <54252267+ManoelCabral1@users.noreply.github.com>
2021-11-11 18:29:39 -0300

    Add files via upload

---
+++ Facebook_Scraper.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+import time
+import pandas as pd
+import sys
+from pathlib import Path
+from Facebot import Face_bot
+
+"""Argumentos para coleta:
+     1. post_lik -> link do POST
+     2. data_name -> nome do arquivo para salvar os dados
+     3. count -> número de clicks no botão mais comentários"""
+
+post_link = sys.argv[1]
+
+data_name = sys.argv[2]
+
+count = int(sys.argv[3])
+
+fname = str(Path(r'C:\Users\EBMquintto\Desktop\Scrapers\facebook_scraper\Dados') / data_name) + '.xlsx'
+
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(r"C:\Users\EBMquintto\Documents\chromedriver\chromedriver.exe", options=options)
+
+driver.get("https://www.facebook.com/")
+
+time.sleep(2)
+bot = Face_bot(driver, post_link)
+
+#login
+bot.login('manoel.cabral1989@outlook.com', 'tobias@123')
+time.sleep(10)
+
+users, comments = bot.load_more_comment(count)
+
+data = pd.DataFrame(columns=['usuário', 'comentário'])
+
+data['usuário'] = pd.Series(users)
+data['comentário'] = pd.Series(comments)
+
+data.to_excel(fname, sheet_name='comentários', index=False)
+
+print(f'Total extraido: {len(comments)}')
+
+bot.close()
